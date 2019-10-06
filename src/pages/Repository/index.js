9af9dd0076798d60e8bd1caccf3e9fde89d48f07
LORDBABUINO <jorge.x7@gmail.com>
2019-10-05 23:26:27 -0300

    Requests repository details

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import api from '../../services/api'
 
-const Repository = ({ match }) => (
-  <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
-)
+class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const repoName = decodeURIComponent(this.props.match.params.repository)
+
+    const [{ data: repository }, { data: issues }] = await Promise.all([
+      await api.get(`/repos/${repoName}`),
+      await api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ])
+    this.setState({
+      repository,
+      issues,
+      loading: false,
+    })
+    console.log({ repository })
+    console.log({ issues })
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state
+    return <h1>Repository: {decodeURIComponent(repository)}</h1>
+  }
+}
 
 export default Repository
