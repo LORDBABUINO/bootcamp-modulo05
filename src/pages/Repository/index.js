d4660b658c0d38c4eb840328227970c0aa207c8d
LORDBABUINO <jorge.x7@gmail.com>
2019-10-05 23:59:58 -0300

    Shows all repository details

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import api from '../../services/api'
 
+import Container from '../../components/container'
+import { Loading, Owner } from './styles'
+
 class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  }
+
   state = {
     repository: {},
     issues: [],
@@ -31,7 +44,18 @@ class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state
-    return <h1>Repository: {decodeURIComponent(repository)}</h1>
+    return loading ? (
+      <Loading>Carregando</Loading>
+    ) : (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    )
   }
 }
 
